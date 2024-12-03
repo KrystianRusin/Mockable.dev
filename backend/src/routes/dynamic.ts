5db4b043cc3db173b524183373438cb12fc956bb
Krystian Rusin <krystian.r@hotmail.com>
2024-12-03 11:51:02 -0500

    implemented POST data generatation

--- backend/src/routes/dynamic.ts
+++ backend/src/routes/dynamic.ts
@@ -34,22 +34,26 @@ router.get("/:userslug/*", async (req: Request, res: Response) => {
 router.post("/:userslug/*", async (req: Request, res: Response) => {
     const userSlug = req.params.userslug;
     const endpointPath = req.params[0];
-
     const url = "/" + endpointPath;
     const method = req.method.toUpperCase();
-
+    const forceRefresh = req.query.forceRefresh === 'true';
+  
     try {
-        const endpoint = await Endpoint.findOne({userSlug, url, method});
-        if(!endpoint){
-            return res.status(404).json({message: 'Endpoint not found'});
-        }
-        const data = await generatePOSTData(endpoint, req.body);
-        res.status(201).json(data);
-        
-    } catch (err) {
-        console.error('Internal Server Error', err);
-        res.status(500).json({message: 'Server Error'});
+      const endpoint = await Endpoint.findOne({ userSlug, url, method });
+      if (!endpoint) {
+        return res.status(404).json({ message: 'Endpoint not found' });
+      }
+      const data = await generatePOSTData(endpoint, req.body, forceRefresh);
+      res.status(201).json(data);
+    } catch (err: any) {
+      console.error('Error handling POST request', err);
+      if (err.message.includes('does not match')) {
+        res.status(400).json({ message: err.message });
+      } else {
+        res.status(500).json({ message: 'Server Error' });
+      }
     }
-});
+  });
+  
 
 export default router;
