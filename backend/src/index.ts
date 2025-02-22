d3824511646e6f650d6a1c9e2a3f419f3898eb44
Krystian Rusin <krystian.r@hotmail.com>
2025-02-21 20:44:43 -0500

    Test Backend CI/CD

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,6 +43,7 @@ app.use("/api/health", healthRoutes)
 const PORT: number = parseInt(process.env.PORT || '5000', 10);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log("Updated Correctly")
 });
 
 process.on('SIGINT', () => {
