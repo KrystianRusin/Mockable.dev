08cdd4d75f17d5253f5ae86a6468b289647b0217
Krystian Rusin <krystian.r@hotmail.com>
2025-02-27 14:31:12 -0500

    updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,14 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: false
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
